7ec1ddd4aa267e980b21d751c0248d589e03520f
teamhair <grupoteamhair@gmail.com>
2020-08-13 08:19:50 -0500

    Updates

--- scripts/script_segura.js
+++ scripts/script_segura.js
@@ -4,6 +4,7 @@ function chageInfoTHSuba (){
 	fotos_sede.src			='./pics/teamhair_avenida_suba/team_hair_avenida_suba_1.jpg';
 	mapa_sede.src 			="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1988.2041675332896!2d-74.0711322146021!3d4.698907936717886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9ad0decbc7eb%3A0xf81a9b1e5af0688c!2sTeam%20Hair%20Peluquer%C3%ADa!5e0!3m2!1ses-419!2sco!4v1595206823778!5m2!1ses-419!2sco";
 	agenda_cita.href		="https://my.enjoycol.com/salon/16";
+	open_whatsapp.href		="https://api.whatsapp.com/send?phone=573138209674&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Team%20Hair%02."
 	whatsapp_number.textContent = "310 7831725"; 
 }		
 
@@ -13,6 +14,7 @@ function chageInfoTHSalitre (){
 	fotos_sede.src			='./pics/teamhair_salitre_ciudadela_sarmiento_angulo/teamhair_salitre_ciudadela_sarmiento_angulo_1.jpg';
 	mapa_sede.src 			="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15906.85052736035!2d-74.1004198!3d4.6452591!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8c6e60a96f9593a3!2sTEAM%20HAIR%20TORRE%203%20PELUQUERIA!5e0!3m2!1ses-419!2sco!4v1595199137470!5m2!1ses-419!2sco";
 	agenda_cita.href		="https://my.enjoycol.com/salon/17";
+	open_whatsapp.href		="https://api.whatsapp.com/send?phone=573123843278&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Team%20Hair%02."
 	whatsapp_number.textContent = "310 7827644"; 
 }
 
@@ -22,6 +24,7 @@ function chageInfoTHMazuren (){
 	fotos_sede.src			='./pics/teamhair_mazuren/teamhair_mazuren_1.jpg';
 	mapa_sede.src 			="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15904.716776742895!2d-74.0548112!3d4.7389065!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1d412dfa9cd8a672!2sTeam%20Hair!5e0!3m2!1ses-419!2sco!4v1595261533066!5m2!1ses-419!2sco";
 	agenda_cita.href		="https://my.enjoycol.com/salon/11";
+	open_whatsapp.href		="https://api.whatsapp.com/send?phone=573124358243&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Team%20Hair%02."
 	whatsapp_number.textContent = "310 7835152"; 
 }
 
@@ -31,6 +34,7 @@ function chageInfoTHColina (){
 	fotos_sede.src			='./pics/teamhair_exito_colina/teamhair_exito_colina_1.jpg';
 	mapa_sede.src 			="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15904.808897873236!2d-74.0659987!3d4.7349016!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x335eb249c7a6f1db!2s%C3%89xito%20Wow%20Colina!5e0!3m2!1ses-419!2sco!4v1595207497382!5m2!1ses-419!2sco";
 	agenda_cita.href		="https://my.enjoycol.com/salon/13";
+	open_whatsapp.href		="https://api.whatsapp.com/send?phone=573202012634&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Team%20Hair%02."
 	whatsapp_number.textContent = "310 7814591"; 
 }
 
@@ -40,6 +44,7 @@ function chageInfoTHCountry (){
 	fotos_sede.src			='./pics/teamhair_exito_country/teamhair_exito_country_1.jpg';
 	mapa_sede.src 			="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15905.309921656968!2d-74.0332488!3d4.7130605!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7c045f877b610aa0!2s%C3%89xito%20Wow%20Country!5e0!3m2!1ses-419!2sco!4v1595208154516!5m2!1ses-419!2sco";
 	agenda_cita.href		="https://my.enjoycol.com/salon/14";
+	open_whatsapp.href		="https://api.whatsapp.com/send?phone=573112856120&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Team%20Hair%02."
 	whatsapp_number.textContent = "310 7815151"; 
 }
 
@@ -49,6 +54,7 @@ function chageInfoTHGranEstacion (){
 	fotos_sede.src			= './pics/teamhair_exito_gran_estacion/teamhair_exito_gran_estacion_1.jpg';
 	mapa_sede.src 			= "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15906.982172583574!2d-74.098869!3d4.6394196!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc5d7b19b8ec10142!2s%C3%89xito%20Gran%20Estacion!5e0!3m2!1ses-419!2sco!4v1595258886559!5m2!1ses-419!2sco" ;
 	agenda_cita.href		="https://my.enjoycol.com/salon/15";
+	open_whatsapp.href		="https://api.whatsapp.com/send?phone=573118576819&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Team%20Hair%02."
 	whatsapp_number.textContent = "310 7814959";
 }
 
